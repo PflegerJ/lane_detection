50196f34a372a732307c45055c22593e48962a0d
frankenweenie <anyamixon@gmail.com>
2020-06-06 14:04:44 -0700

    Adds a test video for lane detection. Uses VideoCapture in a demo method to play the video through frame by frame using OpenCV.

--- HPC490_LaneDetection/HPC490_LaneDetection.cpp
+++ HPC490_LaneDetection/HPC490_LaneDetection.cpp
@@ -9,15 +9,59 @@
 using namespace cv;
 using namespace std;
 
+void drawTwoCirclesDemo();
+void videoPlaybackDemo();
+
 int main()
 {
-    cout << "Hello World!\n";
+    cout << "Press any key to end a demo.\n";
 
+	videoPlaybackDemo();
+
+	drawTwoCirclesDemo();
+
+	return 0;
+
+}
+
+void drawTwoCirclesDemo()
+{
 	Mat image = Mat::zeros(300, 600, CV_8UC3);
 	circle(image, Point(250, 150), 100, Scalar(0, 255, 128), -100);
 	circle(image, Point(350, 150), 100, Scalar(255, 255, 255), -100);
-	imshow("yay", image);
+	imshow("Two Cool Circles!", image);
 	waitKey(0);
-	return 0;
+}
 
+void videoPlaybackDemo()
+{
+	Mat frame;
+	VideoCapture cap;
+	cap.open("laneTest1.mp4");
+
+	// Check if we succeeded
+	if (!cap.isOpened()) 
+	{
+		cerr << "ERROR! Unable to open camera\n";
+		return;
+	}
+
+	cout << "Start grabbing" << endl << "Press any key to terminate" << endl;
+	
+	for (;;)
+	{
+		// Wait for a new frame from camera and store it into 'frame'
+		cap.read(frame);
+		// Check if we succeeded
+		if (frame.empty()) {
+			cerr << "ERROR! blank frame grabbed\n";
+			break;
+		}
+		// Show live and wait for a key with timeout long enough to show images
+		imshow("Live", frame);
+		if (waitKey(33) >= 0)  // 30 fps
+			break;
+	}
+
+	// The camera is deinitialized automatically in VideoCapture destructor
 }
